5f2f4187bc953fbdc5b44445a3c77b232e79a34a
lpwoolcock <lpwoolcock@student.unimelb.edu.au>
2022-01-31 16:14:35 +1100

    - Simplified branch impedance calculation based on Az transformation

--- tf_s2dq.m
+++ tf_s2dq.m
@@ -1,31 +1,15 @@
-function [ZY_dq] = tf_s2dq(tf_s, omega_grid, omega_s)
-    % takes symbolic expr in terms of 's', 
-    
-    syms s J
-    
-    tf_dq = subs(tf_s, s, s + J*omega_s);
-    
-    [tf_dq_num, tf_dq_den] = numden(tf_dq);
-    
-    tf_dq_num_coeffs = coeffs(tf_dq_num, J);
-    tf_dq_den_coeffs = coeffs(tf_dq_den, J);
+function [Gdq] = tf_s2dq(Gs, omega_grid, omega_s)
+    % Gs is stationary frame balanced impedance in terms of 's'
+    % omega_grid is dq frequency grid
+    % omega_s is dq frame frequency
 
-    tf_dq_num = 0;
-    for k=1:length(tf_dq_num_coeffs)
-        tf_dq_num = tf_dq_num + tf_dq_num_coeffs(k)*[0 -1; 1 0]^(k-1);
-    end
-    
-    tf_dq_den = 0;
-    for k=1:length(tf_dq_den_coeffs)
-        tf_dq_den = tf_dq_den + tf_dq_den_coeffs(k)*[0 -1; 1 0]^(k-1);
-    end
-    
-    tf_dq_sym = tf_dq_num / tf_dq_den;
+    syms s 
 
-    ZY_dq = zeros(2, 2, length(omega_grid));
-    
-    for k = 1:length(omega_grid)
-        ZY_dq(:,:,k) = subs(tf_dq_sym, s, 1j * omega_grid(k));
-    end
+    Gpn = zeros(2,2,length(omega_grid));
+    Gpn(1,1,:) = double(subs(Gs, s, 1j * (omega_grid + omega_s)));
+    Gpn(2,2,:) = double(subs(Gs, s, 1j * (omega_grid - omega_s)));
+
+    Az = (1/sqrt(2)) * [1 1j; 1 -1j];
+    Gdq = pagemtimes(Az', pagemtimes(Gpn, Az));
 end
 
